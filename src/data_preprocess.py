d97969539f254b0c92cd25ce1ac385dc42bf5f3b
minindu0 <mw8511@live.com>
2023-07-10 17:37:42 -0500

    get data and batch processing

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -1,24 +1,64 @@
-def get_train_data():
-    file = open("data/test_file.txt", "r")
+import torch
+
+def get_data(path, start, len):
+    file = open(path, "r")
+    label_tensor = torch.empty((len, 1), dtype = torch.float32)
+    input_tensor = torch.empty((len, 8), dtype = torch.float32)
+    index = 0
+    tensor_pos = 0
     for line in file:
+        # only get the specified section in file
+        if index < start:
+            index += 1
+            continue
+        elif index > start and index >= start + len:
+            break
         if line != "\n":
             split_txt = line.split(" ")
-            #print(split_txt)
-            result = split_txt[0]
-            base_0 = split_txt[1][0]
-            base_1 = split_txt[1][1]
-            base_2 = split_txt[1][2]
-            quality = split_txt[3]
-            char_remov = ["]", "[", ",", "\n"]
+            # get three base context in float
+            base_0 = get_corrosponding_float_for_base_character(split_txt[1][0])
+            base_1 = get_corrosponding_float_for_base_character(split_txt[1][1])
+            base_2 = get_corrosponding_float_for_base_character(split_txt[1][2])
+            # get quality in float
+            quality = float(split_txt[3])
+            # get the num of parallel bases in float
             parallel_0 = split_txt[6]
             parallel_1 = split_txt[7]
             parallel_2 = split_txt[8]
             parallel_3 = split_txt[9]
+            char_remov = ["]", "[", ",", "\n"]
             for char in char_remov:
                 parallel_0 = parallel_0.replace(char, "")
                 parallel_1 = parallel_1.replace(char, "")
                 parallel_2 = parallel_2.replace(char, "")
                 parallel_3 = parallel_3.replace(char, "")
-            print(result, base_0, base_1, base_2, quality, parallel_0, parallel_1, parallel_2, parallel_3)
+            parallel_0 = float(parallel_0)
+            parallel_1 = float(parallel_1)
+            parallel_2 = float(parallel_2)
+            parallel_3 = float(parallel_3)
+            #print(result, base_0, base_1, base_2, quality, parallel_0, parallel_1, parallel_2, parallel_3)
+            # append to result tensor,
+            result = split_txt[0] 
+            if result == "false":
+                label_tensor[tensor_pos] = torch.tensor([[0.99]])
+            else:
+                label_tensor[tensor_pos] = torch.tensor([[0.00]])
+            # make and append to the input tensor,
+            input_tensor[tensor_pos] = torch.tensor([[base_0, base_1, base_2, quality, parallel_0, parallel_1, parallel_2, parallel_3]])
+            if index % 10000 == 0:
+                print(index)
+            index += 1
+            tensor_pos += 1
     file.close()
-    return (result, base_0, base_1, base_2, quality, parallel_0, parallel_1, parallel_2, parallel_3)
+    return (input_tensor, label_tensor)
+
+def get_corrosponding_float_for_base_character(character):
+    if character == "A":
+        return_value = 0.0
+    elif character == "C":
+        return_value = 0.33
+    elif character == "G":
+        return_value = 0.66
+    elif character == "T":
+        return_value = 1.0
+    return return_value
--- src/main.py
+++ src/main.py
@@ -3,68 +3,56 @@ import numpy as np
 import random
 import model
 from torch.autograd import Variable
-from data_preprocess import get_train_data
+from data_preprocess import get_data
+import random
 
-get_train_data()
-# Normalize the input
-# Data input: threebase context(1,1,1), pacbio quality(1), and topology bases(1,1,1,1).
-# Test data
-# Setting weights and bias
-w = torch.tensor([[0.1], [0.3], [0.1], [0.5], [0.01], [0.03], [0.02], [0.05]], requires_grad=True)
-b = torch.tensor([[0.01]], requires_grad=True)
-# Defining our forward function for prediction
-def forward(x):
-    # using .mm module for matrix multiplication 
-    y_pred = torch.mm(x, w) + b
-    return y_pred
- 
-# define a tensor 'X' with multiple rows
-X = torch.tensor([[0.33, 0.33, 0.66, 0.75, 5, 7, 3, 2],
-                  [0.66, 0, 0.66, 0.89, 5, 3, 3, 2], 
-                  [0.66, 1, 0.33, 0.93, 6, 6, 3, 10]])
- 
-# Making predictions for Multi-Dimensional tensor "X"
-y_pred = forward(X)
-print("Predictions for 'X': ", y_pred)
+# get the train data
+train_data_start = random.randint(0, 8_000_000)
+train_data_len = 1_000_000
+print("Getting", train_data_len, "train data from", train_data_start, "~")
+(train_inputs, train_labels) = get_data("data/test_file.txt", train_data_start, train_data_len)
 
-# create dummy data for training
-x_test = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
-for i in range(20):
-    x_test = np.append(x_test, [[random.uniform(0.0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 10), random.uniform(0.0, 10), random.uniform(0.0, 10), random.uniform(0.0, 10)]], axis=0)
-print(x_test)
-x_train = torch.from_numpy(np.float32(x_test))
-y_train = forward(x_train)
-print(y_train)
+# get the test data
+test_data_start = random.randint(0, 9_000_000)
+test_data_len = 10
+print("Getting", test_data_len, "train data from", test_data_start, "~")
+(test_inputs, test_labels) = get_data("data/test_file.txt", test_data_start, test_data_len)
 
-# # train the model 
-learningRate = 0.001 
-epochs = 100
+# converting inputs and labels to Variable
+train_inputs = Variable(train_inputs)
+train_labels = Variable(train_labels)
+
+# train the model
+learningRate = 0.00001
+epochs = 10
+batch_size = 1000
 
 # build the model object
 lr_model = model.LR(8, 1)
 # optimizer 
 optimizer = torch.optim.SGD(lr_model.parameters(), lr=learningRate)
+criterion = torch.nn.MSELoss()
 
 for epoch in range(epochs):
-    # Converting inputs and labels to Variable
-    inputs = Variable(x_train)
-    labels = Variable(y_train)
+    permutation = torch.randperm(train_inputs.size()[0])
+    for i in range(0, train_inputs.size()[0], batch_size):
+        # clear gradient buffers 
+        optimizer.zero_grad()
+        # get the batch 
+        indices = permutation[i: i + batch_size]
+        batch_inputs, batch_labels = train_inputs[indices], train_labels[indices]
+        # get output from the model, given the inputs
+        outputs = lr_model(batch_inputs)
+        # get loss for the predicted output
+        #print(outputs)
+        loss = criterion(outputs, batch_labels)
+        # get gradients w.r.t to parameters
+        loss.backward()
+        # update parameters
+        optimizer.step()
+        print('epoch {}, loss {}, batch {}/{}'.format(epoch, loss.item(), i, train_inputs.size()[0]))
 
-    # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
-    optimizer.zero_grad()
-    # get output from the model, given the inputs
-    outputs = lr_model(inputs)
+print("test!")
+for i in range(len(test_inputs)):
+    print("input {} output {} label {}".format(test_inputs[i], lr_model(test_inputs[i]).item(), test_labels[i].item()))
 
-    # get loss for the predicted output
-    criterion = torch.nn.MSELoss() 
-    loss = criterion(outputs, labels)
-    # get gradients w.r.t to parameters
-    loss.backward()
-    # update parameters
-    optimizer.step()
-
-    print('epoch {}, loss {}'.format(epoch, loss.item()))
-
-# make predictions for multiple input samples of 'X'
-y_pred  = lr_model(X)
-print("Predictions for 'X': ", y_pred)
