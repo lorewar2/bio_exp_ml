a998247aee2a9b748958e9879b587e651da5747d
minindu0 <mw8511@live.com>
2023-08-07 14:31:38 -0500

    dataloader in training

--- src/main.py
+++ src/main.py
@@ -12,20 +12,10 @@ import math
 PATH = "./result/model/chr1_1bil_model.pt"
 
 def main():
-    train_dataset = QualityDataset ("data/train_file.txt", "data/train_file.idx")
-    train_loader = DataLoader (
-        dataset = train_dataset,
-        batch_size = 16,
-        shuffle = False,
-        drop_last = True
-    )
-    for batch_idx, (x, y) in enumerate(train_loader):
-        print(batch_idx)
-        print(x[0])
     # set the seed
     torch.manual_seed(0)
     random.seed(2)
-    #train_model()
+    train_model()
     #evaluate_model()
     #test()
     return
@@ -65,21 +55,19 @@ def evaluate_model():
 
 # this function will train the model using the train data
 def train_model():
-    # get the train data (1_000_000 normal)
-    (train_inputs, train_labels) = get_data("data/train_file.txt", 0, 10_000_000, False)
-
-    # get the test data (random test data)
-    (test_inputs, test_labels) = get_data("data/test_file.txt", random.randint(0, 1_000_000), 100, True)
-
-    # converting inputs and labels to Variable
-    train_inputs = Variable(train_inputs)
-    train_labels = Variable(train_labels)
-
     # train parameters
     learningRate = 0.0001
     epochs = 10
     batch_size = 1024
-
+    
+    # data loading
+    train_dataset = QualityDataset ("data/train_file.txt", "data/train_file.idx")
+    train_loader = DataLoader (
+        dataset = train_dataset,
+        batch_size = batch_size,
+        shuffle = True,
+        drop_last = True
+    )
     # build the model object
     lr_model = model.quality_model()
 
@@ -93,18 +81,12 @@ def train_model():
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     epoch = checkpoint['epoch']
     loss = checkpoint['loss']
-
-    # train the model
+    num_batches = len(train_loader)
+    # train loop
     for epoch in range(epochs):
-        val_outputs = lr_model(test_inputs)
-        val_loss = criterion(val_outputs, test_labels)
-        permutation = torch.randperm(train_inputs.size()[0])
-        for i in range(0, train_inputs.size()[0], batch_size):
+        for batch_idx, (batch_inputs, batch_labels) in enumerate(train_loader):
             # clear gradient buffers 
             optimizer.zero_grad()
-            # get the batch 
-            indices = permutation[i: i + batch_size]
-            batch_inputs, batch_labels = train_inputs[indices], train_labels[indices]
             # get output from the model, given the inputs
             outputs = lr_model(batch_inputs)
             # get loss for the predicted output
@@ -113,14 +95,11 @@ def train_model():
             loss.backward()
             # update parameters
             optimizer.step()
-            print('epoch {}, loss {}, val_loss {} batch {}/{}'.format(epoch, loss.item(), val_loss, i, train_inputs.size()[0]))
-
+            print('epoch {}, loss {}, batch {}/{}'.format(epoch, loss.item(), batch_idx, num_batches))
     # save the trained model
     torch.save({'epoch': epoch, 'model_state_dict': lr_model.state_dict(), 'optimizer_state_dict': optimizer.state_dict(), 'loss': loss}, PATH)
-
-    print("test!")
-    for i in range(len(test_inputs)):
-        print("input {} pacbio_qual {} output {}  label {}".format(test_inputs[i][12:], test_inputs[i][12].item(), lr_model(test_inputs)[i].item(), test_labels[i].item()))
+    #for i in range(len(test_inputs)):
+        #print("input {} pacbio_qual {} output {}  label {}".format(test_inputs[i][12:], test_inputs[i][12].item(), lr_model(test_inputs)[i].item(), test_labels[i].item()))
 
 if __name__ == "__main__":
     main()
