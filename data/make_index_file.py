ea7332af338eab053f00fd3785fac8df1de8f826
minindu0 <mw8511@live.com>
2023-08-09 14:28:37 -0500

    model take 64 encoding, better indexer

--- data/make_index_file.py
+++ data/make_index_file.py
@@ -3,26 +3,20 @@ read_path = "./train_file.txt"
 write_path = "./train_file.idx"
 
 # read the offsets of the file lines
-line_offset = [0]
+read_line_offset = [0]
 index = 1
-with open(read_path) as f:
-    line = f.readline()
-    while line:
-        if line == "\n":
-            line_offset.append(f.tell())
-            if index % 1_000_000 == 0:
-                print(index)
-            index += 1
-        line = f.readline()
-
-# write the offsets of the file lines in index
-index = 0
-with open(write_path, "a") as f:
-    for offset in line_offset:
-        write_line = "{:021d} {:021d}\n".format(index, offset)
-        f.write(write_line)
-        index += 1
-        if index % 1_000_000 == 0:
-            print(index)
-
-# find the location 
+with open(read_path) as fr:
+    with open(write_path) as fw:
+        read_line = fr.readline()
+        while read_line:
+            if read_line == "\n":
+                read_line_offset.append(fr.tell())
+                if index % 1_000_000 == 0:
+                    for offset in read_line_offset:
+                        write_line = "{:021d} {:021d}\n".format(index, offset)
+                        fw.write(write_line)
+                        index += 1
+                    read_line_offset.clear()
+                    print("indexed {} records".format(index))
+                index += 1
+            read_line = fr.readline()
