c27c5e151f0928b378832a1639e35628c398147b
minindu0 <mw8511@live.com>
2023-08-15 14:24:34 -0500

    topology score recalculation done

--- src/util.py
+++ src/util.py
@@ -25,23 +25,73 @@ def print_topology_cut_scores():
     print(all_counts)
     return
 
-def calculate_topology_score(calling_base, base_A_count, base_C_count, base_G_count, base_T_count, num_of_reads):
-    ln_prob_base_A =  np.log(0.25)
+def calculate_topology_score(calling_base, base_A_count, base_C_count, base_G_count, base_T_count, num_of_reads, prob):
+    ln_prob_base_A = np.log(0.25)
     ln_prob_base_C = np.log(0.25)
     ln_prob_base_G = np.log(0.25)
     ln_prob_base_T = np.log(0.25)
     
-    ln_prob_data_given_A = np.log(calculate_binomial(num_of_reads, base_A_count))
-    ln_prob_data_given_C = np.log(calculate_binomial(num_of_reads, base_C_count))
-    ln_prob_data_given_G = np.log(calculate_binomial(num_of_reads, base_G_count))
-    ln_prob_data_given_T = np.log(calculate_binomial(num_of_reads, base_T_count))
-    return
+    ln_prob_data_given_A = np.log(calculate_binomial(num_of_reads, base_A_count, prob))
+    ln_prob_data_given_C = np.log(calculate_binomial(num_of_reads, base_C_count, prob))
+    ln_prob_data_given_G = np.log(calculate_binomial(num_of_reads, base_G_count, prob))
+    ln_prob_data_given_T = np.log(calculate_binomial(num_of_reads, base_T_count, prob))
+
+    ln_sum_of_probabilities = ln_prob_data_given_A + ln_prob_base_A
+    ln_sum_of_probabilities = np.logaddexp(ln_sum_of_probabilities, ln_prob_data_given_C + ln_prob_base_C)
+    ln_sum_of_probabilities = np.logaddexp(ln_sum_of_probabilities, ln_prob_data_given_G + ln_prob_base_G)
+    ln_sum_of_probabilities = np.logaddexp(ln_sum_of_probabilities, ln_prob_data_given_T + ln_prob_base_T)
+
+    if calling_base == "A":
+        correct_rate = np.exp(ln_prob_data_given_A + ln_prob_base_A - ln_sum_of_probabilities)
+    elif calling_base == "C":
+        correct_rate = np.exp(ln_prob_data_given_C + ln_prob_base_C - ln_sum_of_probabilities)
+    elif calling_base == "G":
+        correct_rate = np.exp(ln_prob_data_given_G + ln_prob_base_G - ln_sum_of_probabilities)
+    elif calling_base == "T":
+        correct_rate = np.exp(ln_prob_data_given_T + ln_prob_base_T - ln_sum_of_probabilities)
+
+    error_rate = 1.0 - correct_rate
+    quality_score = (-10.00) * np.log10(error_rate + 0.000000000000000000001)
+    #print(quality_score)
+    return quality_score
 
 def calculate_binomial(n, k, prob):
     binomial_coeff = scipy.special.binom(n, k)
-    success = 1
-    failure = 1
+    success = np.power(prob, k)
+    failure = np.power(1.00 - prob, n - k)
     return (binomial_coeff * success * failure)
+
+def pipeline_calculate_topology_score_with_probability():
+    # arrays to save the result
+    error_counts = [0] * 300
+    all_counts = [0] * 300
+    path = "./data/train_file.txt"
+    file = open(path, "r")
+    for index, line in enumerate(file):
+        split_txt = line.split(" ")
+        if len(split_txt) != 11:
+            continue
+        result = split_txt[0]
+        parallel_vec_s = [split_txt[7], split_txt[8], split_txt[9], split_txt[10]]
+        char_remov = ["]", "[", ",", "\n"]
+        for char in char_remov:
+            for index_s in range(len(parallel_vec_s)):
+                temp = parallel_vec_s[index_s].replace(char, "")
+                parallel_vec_s[index_s] = temp
+        parallel_vec_f = []
+        for parallel in parallel_vec_s:
+            parallel_vec_f.append(float(parallel))
+        recalculated_score = int(calculate_topology_score(split_txt[1][1], parallel_vec_f[0], parallel_vec_f[1], parallel_vec_f[2], parallel_vec_f[3], int(split_txt[6]), 0.95))
+        all_counts[recalculated_score] += 1
+        if result == "true":
+            error_counts[recalculated_score] += 1
+        if index % 100000 == 0:
+            print("Running line {}".format(index))
+    print(error_counts)
+    print(all_counts)
+    return
+
+pipeline_calculate_topology_score_with_probability()
 
 def old_data_loader(path, start, length, get_random):
     file = open(path, "r")
