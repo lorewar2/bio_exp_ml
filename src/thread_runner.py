ce8c34e3ad83ea7e291f164c9fa51f51cb7452b8
minindu0 <mw8511@live.com>
2023-10-05 15:13:42 -0500

    use multi process instead

--- src/thread_runner.py
+++ src/thread_runner.py
@@ -1,4 +1,4 @@
-from threading import Thread
+from multiprocessing import Process
 import util
 
 def print_pacbio_scores(read_path, start, end, error_counts, all_counts, thread_index):
@@ -44,15 +44,15 @@ def pipeline_calculate_topology_score_with_probability(read_path, start, end, er
             all_counts[thread_index][recalculated_score] += 1
             if ref_base != call_base:
                 error_counts[thread_index][recalculated_score] += 1
-            if (index - start) % 100001 == 1:
+            if (index - start) % 100001 == 0:
                 print("Thread {} Progress {}/{}".format(thread_index, index - start, end - start))
                 break
     return
     
 # initialize variables
 thread_number = 64
-error_counts = [[0 for i in range(94)] for j in range(thread_number)]
-all_counts = [[0 for i in range(94)] for j in range(thread_number)]
+error_counts = [[0 for i in range(194)] for j in range(thread_number)]
+all_counts = [[0 for i in range(194)] for j in range(thread_number)]
 threads = [None] * thread_number
 file_path = "/data1/hifi_consensus/all_data/chr2_filtered.txt"
 # get the length
@@ -68,7 +68,8 @@ for thread_index in range(len(threads)):
     thread_start = int(one_thread_allocation * thread_index)
     thread_end = int(thread_start + one_thread_allocation)
     # run the thread
-    threads[thread_index] = Thread(target=pipeline_calculate_topology_score_with_probability, args=(file_path, thread_start, thread_end, error_counts, all_counts, 0.85, thread_index))
+    threads[thread_index] = Process(target=pipeline_calculate_topology_score_with_probability, args=(file_path, thread_start, thread_end, error_counts, all_counts, 0.85, thread_index))
+    #threads[thread_index] = Thread(target=pipeline_calculate_topology_score_with_probability, args=(file_path, thread_start, thread_end, error_counts, all_counts, 0.85, thread_index))
     threads[thread_index].start()
 
 # join the threads
@@ -76,12 +77,12 @@ for i in range(len(threads)):
     threads[i].join()
 
 # sum up the result
-all_count_final = [0] * 94
-error_count_final = [0] * 94
+all_count_final = [0] * 194
+error_count_final = [0] * 194
 for i in range(len(all_counts)):
     for j in range(len(all_counts[i])):
-        all_count_final[j] = all_counts[i][j]
-        error_count_final[j] = error_counts[i][j]
+        all_count_final[j] += all_counts[i][j]
+        error_count_final[j] += error_counts[i][j]
 
 print (all_count_final)
 print (error_count_final)
