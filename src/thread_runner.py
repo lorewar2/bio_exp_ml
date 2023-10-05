9d4937cc6c8c264ade62a1c8dd4998a239ee7978
minindu0 <mw8511@live.com>
2023-10-05 14:12:02 -0500

    thread runner 3

--- src/thread_runner.py
+++ src/thread_runner.py
@@ -15,15 +15,15 @@ def print_pacbio_scores(read_path, start, end, error_counts, all_counts, thread_
             all_counts[thread_index][base_quality] += 1
             if ref_base != call_base:
                 error_counts[thread_index][base_quality] += 1
-            if index % 100000 == 0:
-                print("Running line {}".format(index))
+            if index % 100001 == 0:
+                print("Thread {} Progress {}/{}".format(thread_index, index - start, end - start))
                 break
     return
 
-error_counts = [[0] * 94] * 64
-all_counts = [[0] * 94] * 64
-threads = [None] * 64
-results = [None] * 10
+thread_number = 64
+error_counts = [[0 for i in range(94)] for j in range(thread_number)]
+all_counts = [[0 for i in range(94)] for j in range(thread_number)]
+threads = [None] * thread_number
 file_path = "/data1/hifi_consensus/all_data/chr2_filtered.txt"
 # get the length
 total_len = 0
@@ -32,21 +32,17 @@ with open(file_path) as f:
     offset = f.tell()
     total_len = int((offset - 36) / 36)
 one_thread_allocation = total_len / len(threads)
-start = 0
-end = total_len
 print(total_len)
-print_pacbio_scores(file_path, start, end, error_counts, all_counts, 1)
-print(error_counts)
-print(all_counts)
-for i in range(len(threads)):
+for thread_index in range(len(threads)):
     # calculate the start and end of thread
-    print("test")
-    #threads[i] = Thread(target=print_pacbio_scores, args=("/data1/hifi_consensus/all_data/chr2.txt", error_count, all_count, i))
-    #threads[i].start()
+    thread_start = one_thread_allocation * thread_index
+    thread_end = thread_start + one_thread_allocation
+    threads[thread_index] = Thread(target=print_pacbio_scores, args=(file_path, thread_start, thread_end, error_counts, all_counts, thread_index))
+    threads[thread_index].start()
 
-# do some other stuff
-
+# join the threads
 for i in range(len(threads)):
     threads[i].join()
 
-print (" ".join(results))
+print (" ".join(all_counts))
+print (" ".join(error_counts))
