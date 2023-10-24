573f0db3121fdc1d0f83bfce2a9e7ac68630be34
minindu0 <mw8511@live.com>
2023-10-24 17:14:57 -0500

    generic ml model

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,3 +1,4 @@
 from .test_model import quality_model_1_layer
 from .test_model import quality_model_2_layer
 from .test_model import quality_model_3_layer
+from .test_model import quality_model
--- src/model/test_model.py
+++ src/model/test_model.py
@@ -1,5 +1,36 @@
 import torch
+import util
+
 # creating custom modules with package 'nn.Module'
+class quality_model(torch.nn.Module):
+    # Object Constructor
+    def __init__(self, base_context, layer_count):
+        super().__init__()
+        self.layer_count = layer_count
+        # make a layer vector
+        layer_node_count = []
+        # calculate the one hot encoding count
+        one_hot_count = pow(4, base_context)
+        # calibrate first layer
+        layer_node_count.append(one_hot_count + 5)
+        # calibrate the rest of layers
+        for index in range(0, layer_count - 1):
+            layer_node_count.append(200)
+        # calibrate the last layer 1 node
+        layer_node_count.append(1)
+        self.layer_list = []
+        # make the layers
+        for index in range(0, layer_count):
+            self.layer_list.append(torch.nn.Linear(layer_node_count[index], layer_node_count[index + 1], bias = False))
+        self.sig = torch.nn.Sigmoid()
+    # define the forward function for prediction
+    def forward(self, x):
+        sum_tensor = x
+        for index in range(0, self.layer_count):
+            sum_tensor = self.layer_list[index](sum_tensor)
+        result = self.sig(sum_tensor)
+        return result
+
 class quality_model_1_layer(torch.nn.Module):
     # Object Constructor
     def __init__(self):
