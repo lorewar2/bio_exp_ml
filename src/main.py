4bc94bd97a4406c8bd5a83d583a00b73686dfd9d
minindu0 <mw8511@live.com>
2023-11-23 15:37:41 -0600

    debug model

--- src/main.py
+++ src/main.py
@@ -19,6 +19,7 @@ def main():
     random.seed(3)
     np.random.seed(2)
     train_model()
+    #view_result()
     return
 
 # this function will train the model using the train data
@@ -27,22 +28,47 @@ def train_model():
     # train parameters
     learningRate = 0.000001
     epochs = 10
-    batch_size = 1024
+    batch_size = 1
     # data loading
     train_dataset = QualityDataset (DATA_PATH, True, CONTEXT_COUNT)
     train_loader = DataLoader (
         dataset = train_dataset,
         batch_size = batch_size,
-        num_workers = 64,
+        num_workers = 1,
         shuffle = False,
         drop_last = True
     )
     # build the model object
     lr_model = model.quality_model_1_layer(CONTEXT_COUNT, EXTRA_COUNT)
 
+    # define custom weights
+    custom_weight = torch.rand(lr_model.linear.weight.shape)
+    #first_layer_size = tensor_length
+    first_layer_size = 1
+    # calling base count
+    for i in range(0, first_layer_size):
+        custom_weight[i][tensor_length - 4] = torch.tensor(+1.0437)
+        # other base count
+        custom_weight[i][tensor_length - 3] = torch.tensor(-0.2337)
+        custom_weight[i][tensor_length - 2] = torch.tensor(-0.9995)
+        custom_weight[i][tensor_length - 1] = torch.tensor(-1.0)
+        # pacbio qual
+        custom_weight[i][tensor_length - 5] = torch.tensor(+1.0)
+    # put the weights in the model
+    lr_model.linear.weight = torch.nn.Parameter(custom_weight)
+
     # optimizer
     optimizer = torch.optim.SGD(lr_model.parameters(), lr = learningRate)
     criterion = torch.nn.MSELoss()
+
+    torch.save({'epoch': 0, 'model_state_dict': lr_model.state_dict(), 'optimizer_state_dict': optimizer.state_dict(), 'loss': 9999}, MODEL_PATH)
+    # # load the previous saved trained model
+    checkpoint = torch.load(MODEL_PATH)
+    lr_model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+    loss = checkpoint['loss']
+
     num_batches = len(train_loader)
     # train loop
     for epoch in range(epochs):
@@ -130,12 +156,12 @@ def view_result():
         for batch_idx, (batch_inputs, batch_labels) in enumerate(eval_loader):
             pred = lr_model(batch_inputs)
             for i in range(len(batch_inputs)):
-                if (batch_labels[i].item() > 0.9) and (error_tensor_len < required_number):
+                if (batch_labels[i].item() < 0.9) and (error_tensor_len < required_number):
                     pacbio_qual = batch_inputs[i][0][tensor_length - 5].item()
                     if pacbio_qual > 0.001:
                         error_tensor[error_tensor_len] = torch.concat((batch_inputs[i], batch_labels[i], pred[i]), dim = 1)
                         error_tensor_len += 1
-                elif (batch_labels[i].item() < 0.9) and (correct_tensor_len < required_number):
+                elif (batch_labels[i].item() > 0.9) and (correct_tensor_len < required_number):
                     correct_tensor[correct_tensor_len] = torch.concat((batch_inputs[i], batch_labels[i], pred[i]), dim = 1)
                     correct_tensor_len += 1
             print("Processing {}/{} found correct {} error {}".format(batch_idx, eval_len, correct_tensor_len, error_tensor_len))
@@ -153,9 +179,9 @@ def view_result():
 
 
 def print_result_tensor(obtained_tensor):
-    tensor_length = pow(5, CONTEXT_COUNT) + 15
+    tensor_length = pow(5, CONTEXT_COUNT) + EXTRA_COUNT
     # get the three base context from first 64 bits
-    for idx, value in enumerate(obtained_tensor[0: tensor_length - 18]):
+    for idx, value in enumerate(obtained_tensor[0: tensor_length - EXTRA_COUNT]):
         if value > 0.5:
             converted_number = idx
     base_context = util.convert_bits_to_bases(converted_number, CONTEXT_COUNT)
