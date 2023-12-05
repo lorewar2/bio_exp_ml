881561228f0519f76d8bf8322935f80ca132c4ce
minindu0 <mw8511@live.com>
2023-12-05 17:55:56 -0600

    update base correction output

--- src/util.py
+++ src/util.py
@@ -14,8 +14,10 @@ def output_the_base_corrections (data_path):
     total_fix = 0
     poa_fix = 0
     parallel_fix = 0
+    del_fix = 0
     total = [0] * 125
     poa = [0] * 125
+    del1 = [0] * 125
     parallel = [0] * 125
     with open(data_path) as f1:
         for line in f1:
@@ -27,29 +29,31 @@ def output_the_base_corrections (data_path):
                 base_context = [split_txt[6][2], split_txt[6][3], split_txt[6][4]]
                 # get the converted number
                 converted_number = convert_bases_to_bits(base_context, 3)
-                if split_txt[6][3] == split_txt[1][3]:
-                    total[converted_number] += 1
-                else:
+                if split_txt[6][3] != split_txt[1][3]:
                     total_fix += 1
+                    total[converted_number] += 1
                     calling_base, poa_state = get_state_info(split_txt[7])
                     # poa fix
                     if calling_base == split_txt[1][3]:
                         poa_fix += 1
-                        total[converted_number] += 1
                         poa[converted_number] += 1
+                        parallel[converted_number] += 1
+                    elif poa_state[2] > 0.5:
+                        del_fix += 1
+                        del1[converted_number] += 1
                         parallel[converted_number] += 1
                     # parallel fix
                     else:
                         parallel_vec_f = clean_string_get_array([split_txt[14], split_txt[15], split_txt[16], split_txt[17]])
                         sorted_vec = rearrange_sort_parallel_bases(parallel_vec_f, calling_base)
-                        if sorted_vec[1] > sorted_vec[0]:
+                        if (sorted_vec[1] > sorted_vec[0]) or (poa_state[2] > 0.5):
                             parallel_fix += 1
-                            total[converted_number] += 1
                             parallel[converted_number] += 1
             if index % 10000 == 0:
-                print("lines {} total: {} poa: {} parallel: {}".format(index, total_fix, poa_fix, parallel_fix))
+                print("lines {} total: {} poa: {} del: {} parallel: {}".format(index, total_fix, poa_fix, del_fix, parallel_fix))
     print(total)
     print(poa)
+    print(del1)
     print(parallel)
     return
 
